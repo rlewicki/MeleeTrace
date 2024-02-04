40442b67775dd7313f382a5acbf66d7301fb31b1
rlewicki <contact@rlewicki.tech>
2024-02-04 13:44:51 +0100

    Add new source file for common declaration across the plugin; add log category declaration and definition

---
+++ Source/MeleeTrace/Private/MeleeTraceCommon.cpp
@@ -0,0 +1,5 @@
+﻿// Copyright 2024, Robert Lewicki, All rights reserved.
+
+#include "MeleeTraceCommon.h"
+
+DEFINE_LOG_CATEGORY(LogMeleeTrace);
---
+++ Source/MeleeTrace/Public/MeleeTraceCommon.h
@@ -0,0 +1,7 @@
+﻿// Copyright 2024, Robert Lewicki, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogMeleeTrace, Log, All);
