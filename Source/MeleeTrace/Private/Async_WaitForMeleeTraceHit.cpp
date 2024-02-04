589bb7655551be227cb467aa696de56a19b2ba35
rlewicki <contact@rlewicki.tech>
2024-02-04 16:34:50 +0100

    Add cancellable async node waiting for melee trace hit detection

---
+++ Source/MeleeTrace/Private/Async_WaitForMeleeTraceHit.cpp
@@ -0,0 +1,74 @@
+﻿// Copyright 2024, Robert Lewicki, All rights reserved.
+
+#include "Async_WaitForMeleeTraceHit.h"
+
+#include "MeleeTraceCommon.h"
+#include "MeleeTraceComponent.h"
+
+UAsync_WaitForMeleeTraceHit* UAsync_WaitForMeleeTraceHit::WaitForMeleeHit(UObject* WorldContextObject, AActor* ActorToWatch)
+{
+	const UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+	if (!World)
+	{
+		return nullptr;
+	}
+
+	if (!ActorToWatch)
+	{
+		UE_LOG(
+			LogMeleeTrace,
+			Error,
+			TEXT("%s: Invalid reference for actor to watch passed as parameter"), StringCast<TCHAR>(__FUNCTION__).Get());
+		return nullptr;
+	}
+
+	auto* MeleeTraceComponent = ActorToWatch->FindComponentByClass<UMeleeTraceComponent>();
+	if (!MeleeTraceComponent)
+	{
+		UE_LOG(
+			LogMeleeTrace,
+			Error,
+			TEXT("%s: Actor to watch doesn't have MeleeTraceComponent attached"), StringCast<TCHAR>(__FUNCTION__).Get());
+		return nullptr;
+	}
+
+	auto* NewAction = NewObject<UAsync_WaitForMeleeTraceHit>();
+	NewAction->ActorToWatch = ActorToWatch;
+	NewAction->MeleeTraceComponent = MeleeTraceComponent;
+	NewAction->RegisterWithGameInstance(World);
+	return NewAction;
+}
+
+void UAsync_WaitForMeleeTraceHit::Cancel()
+{
+	if (MeleeTraceComponent.IsValid())
+	{
+		MeleeTraceComponent->OnTraceHit.RemoveDynamic(this, &ThisClass::HandleTraceHit);
+	}
+
+	ActorToWatch.Reset();
+	MeleeTraceComponent.Reset();
+
+	Super::Cancel();
+}
+
+void UAsync_WaitForMeleeTraceHit::Activate()
+{
+	MeleeTraceComponent->OnTraceHit.AddDynamic(this, &ThisClass::HandleTraceHit);
+}
+
+void UAsync_WaitForMeleeTraceHit::HandleTraceHit(
+	UMeleeTraceComponent* ThisComponent,
+	AActor* HitActor,
+	const FVector& HitLocation,
+	const FVector& HitNormal,
+	FName HitBoneName)
+{
+	FAsyncMeleeHitInfo HitInfo;
+	HitInfo.OwnerTraceComponent = ThisComponent;
+	HitInfo.HitActor = HitActor;
+	HitInfo.HitLocation = HitLocation;
+	HitInfo.HitNormal = HitNormal;
+	HitInfo.HitBoneName = HitBoneName;
+	OnHit.Broadcast(HitInfo);
+}
---
+++ Source/MeleeTrace/Public/Async_WaitForMeleeTraceHit.h
@@ -0,0 +1,61 @@
+﻿// Copyright 2024, Robert Lewicki, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/CancellableAsyncAction.h"
+#include "Async_WaitForMeleeTraceHit.generated.h"
+
+class UMeleeTraceComponent;
+
+USTRUCT(Blueprintable)
+struct MELEETRACE_API FAsyncMeleeHitInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, Transient)
+	TObjectPtr<UMeleeTraceComponent> OwnerTraceComponent;
+
+	UPROPERTY(BlueprintReadOnly, Transient)
+	TObjectPtr<AActor> HitActor;
+
+	UPROPERTY(BlueprintReadOnly, Transient)
+	FVector HitLocation;
+
+	UPROPERTY(BlueprintReadOnly, Transient)
+	FVector HitNormal;
+
+	UPROPERTY(BlueprintReadOnly, Transient)
+	FName HitBoneName;
+};
+
+UCLASS()
+class MELEETRACE_API UAsync_WaitForMeleeTraceHit : public UCancellableAsyncAction
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "Melee Trace",
+		meta = (WorldContext = "WorldContextObject", BlueprintInternalUseOnly = "True"))
+	static UAsync_WaitForMeleeTraceHit* WaitForMeleeHit(UObject* WorldContextObject, AActor* ActorToWatch);
+
+	virtual void Cancel() override;
+	
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAsyncWaitForMeleeHitDetected, FAsyncMeleeHitInfo, HitInfo);
+	UPROPERTY(BlueprintAssignable)
+	FAsyncWaitForMeleeHitDetected OnHit;
+protected:
+	virtual void Activate() override;
+
+	TWeakObjectPtr<AActor> ActorToWatch;
+	TWeakObjectPtr<UMeleeTraceComponent> MeleeTraceComponent;
+
+	UFUNCTION()
+	void HandleTraceHit(
+		UMeleeTraceComponent* ThisComponent,
+		AActor* HitActor,
+		const FVector& HitLocation,
+		const FVector& HitNormal,
+		FName HitBoneName);
+};
