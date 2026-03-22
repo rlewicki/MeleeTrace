7d2f6907d32bf6525fe4050c77afa80f383e54c3
rlewicki <contact@rlewicki.tech>
2026-03-22 20:41:16 +0100

    Update build script to only support last 3 versions of Unreal

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,19 +1,13 @@
 set PluginPath=MeleeTrace.uplugin
 set OutputPath=%1
 set BuildVersion=%2
-set EnginePath51=%UE_PATH_5_1%
-set EnginePath52=%UE_PATH_5_2%
-set EnginePath53=%UE_PATH_5_3%
-set EnginePath54=%UE_PATH_5_4%
 set EnginePath55=%UE_PATH_5_5%
+set EnginePath56=%UE_PATH_5_6%
+set EnginePath57=%UE_PATH_5_7%
 
-call "%EnginePath51%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.1"
-if %ERRORLEVEL% GEQ 1 EXIT /B 1
-call "%EnginePath52%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.2"
-if %ERRORLEVEL% GEQ 1 EXIT /B 1
-call "%EnginePath53%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.3"
-if %ERRORLEVEL% GEQ 1 EXIT /B 1
-call "%EnginePath54%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.4"
-if %ERRORLEVEL% GEQ 1 EXIT /B 1
 call "%EnginePath55%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.5"
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+call "%EnginePath56%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.6"
+if %ERRORLEVEL% GEQ 1 EXIT /B 1
+call "%EnginePath57%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.7"
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
