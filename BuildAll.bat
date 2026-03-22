d7e0098f255e0f34dd5001b4233d80987a7c5955
rlewicki <contact@rlewicki.tech>
2026-03-22 20:43:25 +0100

    Fix path to the RunUAT.bat file inside the engine directory for build script

--- BuildAll.bat
+++ BuildAll.bat
@@ -5,9 +5,9 @@ set EnginePath55=%UE_PATH_5_5%
 set EnginePath56=%UE_PATH_5_6%
 set EnginePath57=%UE_PATH_5_7%
 
-call "%EnginePath55%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.5"
+call "%EnginePath55%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.5"
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
-call "%EnginePath56%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.6"
+call "%EnginePath56%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.6"
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
-call "%EnginePath57%\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.7"
+call "%EnginePath57%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -plugin="%cd%\%PluginPath%" -package="%OutputPath%\MeleeTrace-%BuildVersion%_Win64_5.7"
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
